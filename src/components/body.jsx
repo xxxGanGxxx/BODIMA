7a7920857d239f59fb0014ffd3fea230e3bba02e
Sjimalka <hasiduimalka4@gmail.com>
2025-09-19 02:56:46 +0530

    corrct one

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -2,19 +2,19 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
 function Body() {
-return (
+  return (
     <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="https://via.placeholder.com/286x180.png?text=Card+Image" alt="Card image" />
-    <Card.Body>
+      <Card.Img variant="top" src="https://via.placeholder.com/286x180.png?text=Card+Image" alt="Card image" />
+      <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
-        Some quick example text to build on the card title and make up the
-        bulk of the card's content.
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
+      </Card.Body>
     </Card>
-);
+  );
 }
 
 export default Body;
